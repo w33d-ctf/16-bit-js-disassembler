c1b2603b9deddb1a781e4802d04f1fc48cf53f14
jw910731 <jw910731@gmail.com>
2022-02-26 16:28:06 +0800

    Add readin instruction binary file

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 from instruction.parser import Instruction
-print(Instruction(0b0000001000000001))
+
+import sys
+
+with open(sys.argv[1], 'rb') as f:
+    inc = 0
+    while True:
+        instruction = f.read(2)
+        if instruction == "":
+            break
+        try:
+            print("{:02x} {}".format(inc, Instruction(
+                int.from_bytes(instruction, byteorder='little'))))
+        except IndexError:
+            print("{:02x} {}".format(inc, instruction))
+        inc += 2
